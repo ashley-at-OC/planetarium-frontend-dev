1f599f0726aae8c79dfc56209c6f252a634be9fe
Ashley <ashphamley@gmail.com>
2026-06-14 17:33:24 -0500

    Removed pasted link that broke routing

--- src/router.js
+++ src/router.js
@@ -33,7 +33,7 @@ const router = createRouter({
     {
       path: "/show/:id",
       name: "selectedShow",
-    https://github.com/ashley-at-OC/planetarium-frontend-dev/pull/20/conflict?name=src%252Fcomponents%252FMenuBar.vue&ancestor_oid=91e3868929218daafb6dd368a7ce7f6d18662f8e&base_oid=bac45a9e7cbc9bf27775dc4a03725e021d3cee6e&head_oid=d662122328d12ac5c6ba7928125637d351cd2b4c  props: true,
+      props: true,
       component: () => import("./views/SelectedShow.vue"),
     },
     {
@@ -52,8 +52,8 @@ const router = createRouter({
       name: "editUser",
       props: true,
       component: () => import("./views/EditUser.vue"),
-},
-                            {
+    },
+    {
       path: "/purchases",
       name: "purchases",
       component: () => import("./views/PurchaseHistory.vue"),
